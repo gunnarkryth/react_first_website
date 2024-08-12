45673e0a90dfa7ed980a4dcb371097208381db11
Gunski <gunnarkryth@gmail.com>
2024-08-12 10:35:11 +0200

    markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { Header } from "./assets/components/Header";
+import { Main } from "./assets/components/Main";
+import { Footer } from "./assets/components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <p>Halløj verden</p>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </>
   );
 }
